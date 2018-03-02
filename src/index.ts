c5e58240d8c15b5a61ba91b80f289af3ab0a7a72
Jonathan Hoyt <hoyt@github.com>
2018-03-01 22:06:28 -0800

    Trying out dynamic import

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,9 @@ import Koa from 'koa'
 const app = new Koa()
 
 app.use(async ctx => {
-  ctx.body = 'Hello World'
+  const { default: data } = await import('./other')
+
+  ctx.body = `Hello World ${Math.random() > 0.5 ? data.a : data.b}`
 })
 
 app.listen(3000)
