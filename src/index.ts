4d7c8698cdc5107d18e2fa147f338336c8dd3b5a
Jonathan Hoyt <hoyt@github.com>
2018-03-01 23:03:16 -0800

    Use dotenv to get environment variables in development

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 import Koa from 'koa'
 import ping from './Routes/ping'
 
@@ -5,10 +7,11 @@ const app = new Koa()
 
 app.use(ping.routes())
 app.use(ping.allowedMethods())
+
 app.use(async ctx => {
   const { default: data } = await import('./other')
 
-  ctx.body = `Hello World ${Math.random() > 0.5 ? data.a : data.b}`
+  ctx.body = `Hello World ${Math.random() > 0.5 ? data.a : data.b} - NODE_ENV: ${process.env.NODE_ENV}`
 })
 
 app.listen(3000)
